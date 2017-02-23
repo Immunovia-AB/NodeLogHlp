4e5345f2c840fe6e0b85cda92cd431e7e3cac181
Martin <martin.jirlow@gmail.com>
2017-02-23 09:47:22 +0100

    Added Console config option and rewrote config loading

--- config.js
+++ config.js
@@ -1,18 +1,55 @@
+var fs = require('fs');
+
+var confMap = {
+    'AWS_DEFAULT_REGION': 'region',
+    'AWS_ACCESS_KEY': 'accessKeyId',
+    'AWS_SECRET_ACCESS_KEY': 'secretAccessKey',
+    'LOGLEVEL': 'logLevel',
+    'LOG_TO_CMD': 'logToCmd'
+};
+
+function getConfigVariable(name, configFile) {
+
+    if (typeof process.env[name] != 'undefined') {
+        return process.env[name];
+    }
+
+    if (configFile) {
+        if (typeof configFile[name] != 'undefined') {
+            return configFile[name];
+        }
+        else if (typeof configFile[confMap[name]] != 'undefined') {
+            return configFile[confMap[name]];
+        }
+    }
+
+    throw {
+        name: "ConfigError",
+        message: "Missing config for variable " + name + ".",
+    }
+}
+
 exports.set = function(app, table, path) {
     global.Appl = app;
     global.Table = table;
-    if (process.env.AWS_DEFAULT_REGION) {    
-        global.AccessKeyId = process.env.AWS_ACCESS_KEY;
-        global.SecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-        global.Region = process.env.AWS_DEFAULT_REGION;
-        global.LogLevel = process.env.LOGLEVEL;
-    } else {
-        var fs = require("fs");
-        var file = fs.readFileSync(path + "/config.json");
-        var config = JSON.parse(file);
-        global.AccessKeyId = config.accessKeyId;
-        global.SecretAccessKey = config.secretAccessKey;
-        global.Region = config.region;
-        global.LogLevel = config.logLevel;
+
+    var config;
+
+    try {
+        config = JSON.parse(fs.readFileSync(path + '/config.json'));
     }
+    catch (err) {
+        if (err.code == "ENOENT") {
+            console.log("No config file found.");
+        }
+        else {
+            throw err;
+        }
+    }
+
+    global.AccessKeyId = getConfigVariable('AWS_ACCESS_KEY', config);
+    global.SecretAccessKey = getConfigVariable('AWS_SECRET_ACCESS_KEY', config);
+    global.Region = getConfigVariable('AWS_DEFAULT_REGION', config);
+    global.LogLevel = getConfigVariable('LOGLEVEL', config);
+    global.LogToCmd = getConfigVariable('LOG_TO_CMD', config);
 }
